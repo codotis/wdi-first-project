fed6cce3922f106b60d8f28a82dabb132cc5edd5
Otis <otisvickersgraver@gmail.com>
2017-12-11 13:02:29 +0000

    created 2 working flashing small triangles and 2 non functional

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
+$(()=> {
 
+  const redFlashColors = ['red-flash-pink', 'red-flash-red', 'red-flash-blue', 'red-flash-green'];
+  const blueFlashColors = ['blue-flash-pink', 'blue-flash-red', 'blue-flash-blue', 'blue-flash-green'];
+  const greenFlashColors = ['green-flash-pink', 'green-flash-red', 'green-flash-blue', 'green-flash-green'];
+  const pinkFlashColors = ['pink-flash-pink', 'pink-flash-red', 'pink-flash-blue', 'pink-flash-green'];
+
+  setInterval(function(){
+    const redRandomColor = redFlashColors[Math.floor(Math.random() * 4)];
+    $('.flashing-red').toggleClass(redRandomColor);
+  }, 500);
+
+  setInterval(function(){
+    const blueRandomColor = blueFlashColors[Math.floor(Math.random() * 4)];
+    $('.flashing-blue').toggleClass(blueRandomColor);
+  }, 500);
+
+  setInterval(function(){
+    const greenRandomColor = greenFlashColors[Math.floor(Math.random() * 4)];
+    $('.flashing-green').toggleClass(greenRandomColor);
+  }, 500);
+
+  setInterval(function(){
+    const pinkRandomColor = pinkFlashColors[Math.floor(Math.random() * 4)];
+    $('.flashing-pink').toggleClass(pinkRandomColor);
+  }, 500);
+
+
+
+
+
+
+
+
+
+
+// const isMatch = $( ".flashing-red" ).hasClass( "backgroundRed" )
+// if(isMatch) {
+
+//} else {
+
+//}
+});
