381c9be0b662a8c5be673d5ce271d78db57f3ec2
Otis <otisvickersgraver@gmail.com>
2017-12-11 21:53:21 +0000

    changed pink divs to yellow

--- js/script.js
+++ js/script.js
@@ -5,42 +5,29 @@ $(()=> {
   const $user = $('#user');
   const $lives = $('#lives');
   const $pause = $('#pause');
-  const $score = $('#score');
-  const $staticred = $('.static-red');
-  const $staticblue = $('.static-blue');
-  const $staticgreen = $('.static-green');
-  const $staticpink = $('.static-pink');
+  const $scorenumber = $('#scorenumber');
   const $flashingred = $('.flashing-red');
   const $flashingblue = $('.flashing-blue');
   const $flashinggreen = $('.flashing-green');
-  const $flashingpink = $('.flashing-pink');
+  const $flashingyellow = $('.flashing-yellow');
 
 
-  const redFlashColors = ['red-flash-pink', 'red-flash-red', 'red-flash-blue', 'red-flash-green'];
-  const blueFlashColors = ['blue-flash-pink', 'blue-flash-red', 'blue-flash-blue', 'blue-flash-green'];
-  const greenFlashColors = ['green-flash-pink', 'green-flash-red', 'green-flash-blue', 'green-flash-green'];
-  const pinkFlashColors = ['pink-flash-pink', 'pink-flash-red', 'pink-flash-blue', 'pink-flash-green'];
+  const redFlashColors = ['red-flash-yellow', 'red-flash-red', 'red-flash-blue', 'red-flash-green'];
+  const blueFlashColors = ['blue-flash-yellow', 'blue-flash-red', 'blue-flash-blue', 'blue-flash-green'];
+  const greenFlashColors = ['green-flash-yellow', 'green-flash-red', 'green-flash-blue', 'green-flash-green'];
+  const yellowFlashColors = ['yellow-flash-yellow', 'yellow-flash-red', 'yellow-flash-blue', 'yellow-flash-green'];
+
 
   setInterval(function(){
     const redRandomColor = redFlashColors[Math.floor(Math.random() * 4)];
-    $('.flashing-red').toggleClass(redRandomColor);
-  }, 1000);
-
-  setInterval(function(){
     const blueRandomColor = blueFlashColors[Math.floor(Math.random() * 4)];
-    $('.flashing-blue').toggleClass(blueRandomColor);
-  }, 1000);
-
-  setInterval(function(){
     const greenRandomColor = greenFlashColors[Math.floor(Math.random() * 4)];
+    const yellowRandomColor = yellowFlashColors[Math.floor(Math.random() * 4)];
+    $('.flashing-red').toggleClass(redRandomColor);
+    $('.flashing-blue').toggleClass(blueRandomColor);
     $('.flashing-green').toggleClass(greenRandomColor);
-
-  }, 1000);
-
-  setInterval(function(){
-    const pinkRandomColor = pinkFlashColors[Math.floor(Math.random() * 4)];
-    $('.flashing-pink').toggleClass(pinkRandomColor);
-  }, 1000);
+    $('.flashing-yellow').toggleClass(yellowRandomColor);
+  }, 200);
 
 
   $instructions.on('click', function() {
@@ -51,51 +38,60 @@ $(()=> {
   });
 
   function checkRedMatch() {
-    const redMatch = $flashingred.hasClass('.red-flash-red');
+    const redMatch = $flashingred.hasClass('red-flash-red');
     if (redMatch === true) {
-      console.log('1000');
+      score();
     }
   }
 
   function checkBlueMatch() {
-    const blueMatch = $flashingblue.hasClass('.blue-flash-blue');
+    const blueMatch = $flashingblue.hasClass('blue-flash-blue');
     if (blueMatch === true) {
-      console.log('1000');
+      score();
     }
   }
 
   function checkGreenMatch() {
-    const greenMatch = $flashinggreen.hasClass('.green-flash-green');
+    const greenMatch = $flashinggreen.hasClass('green-flash-green');
     if (greenMatch === true) {
-      console.log('1000');
+      score();
+    }
+
+  }
+
+  function checkYellowMatch() {
+    const yellowMatch = $flashingyellow.hasClass('yellow-flash-yellow');
+    if (yellowMatch === true) {
+      score();
     }
   }
 
-  function checkPinkMatch() {
-    const pinkMatch = $flashingpink.hasClass('.pink-flash-pink');
-    if (pinkMatch === true) {
-      console.log('1000');
-    }
-  }
+
 
   $(document).keydown(function(e) {
     if (e.keyCode === 37) {
-      checkPinkMatch();
+      e.preventDefault();
+      checkYellowMatch();
     }
     if (e.keyCode === 38) {
+      e.preventDefault();
       checkRedMatch();
     }
     if (e.keyCode === 39) {
+      e.preventDefault();
       checkBlueMatch();
     }
     if (e.keyCode === 40) {
+      e.preventDefault();
       checkGreenMatch();
     }
   });
 
+  function score() {
+    let playerScore = $scorenumber.text();
 
 
-
+  }
 
 
 
