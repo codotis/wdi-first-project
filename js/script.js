b2ba1972bc84e403d23726d91420642e6f9a59a5
Otis <otisvickersgraver@gmail.com>
2017-12-11 14:32:21 +0000

    added const jquery elements and start of click function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 $(()=> {
 
+  const $instructions = $('#instr');
+  const $startgame = $('#start');
+  const $user = $('#user');
+  const $lives = $('#lives');
+  const $pause = $('#pause');
+  const $score = $('#score');
+  const $staticred = $('.static-red');
+  const $staticblue = $('.static-blue');
+  const $staticgreen = $('.static-green');
+  const $staticpink = $('.static-pink');
+  const $flashingred = $('.flashing-red');
+  const $flashingblue = $('.flashing-blue');
+  const $flashinggreen = $('.flashing-green');
+  const $flashingpink = $('.flashing-pink');
+
+
   const redFlashColors = ['red-flash-pink', 'red-flash-red', 'red-flash-blue', 'red-flash-green'];
   const blueFlashColors = ['blue-flash-pink', 'blue-flash-red', 'blue-flash-blue', 'blue-flash-green'];
   const greenFlashColors = ['green-flash-pink', 'green-flash-red', 'green-flash-blue', 'green-flash-green'];
@@ -10,20 +26,20 @@ $(()=> {
     $('.flashing-red').toggleClass(redRandomColor);
   }, 500);
 
-  setInterval(function(){
-    const blueRandomColor = blueFlashColors[Math.floor(Math.random() * 4)];
-    $('.flashing-blue').toggleClass(blueRandomColor);
-  }, 500);
+  // setInterval(function(){
+  //   const blueRandomColor = blueFlashColors[Math.floor(Math.random() * 4)];
+  //   $('.flashing-blue').toggleClass(blueRandomColor);
+  // }, 500);
 
   setInterval(function(){
     const greenRandomColor = greenFlashColors[Math.floor(Math.random() * 4)];
     $('.flashing-green').toggleClass(greenRandomColor);
   }, 500);
 
-  setInterval(function(){
-    const pinkRandomColor = pinkFlashColors[Math.floor(Math.random() * 4)];
-    $('.flashing-pink').toggleClass(pinkRandomColor);
-  }, 500);
+  // setInterval(function(){
+  //   const pinkRandomColor = pinkFlashColors[Math.floor(Math.random() * 4)];
+  //   $('.flashing-pink').toggleClass(pinkRandomColor);
+  // }, 500);
 
 
 
