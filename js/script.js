66a26165d4681be5267eade5b8370dd91c70f686
Otis <otisvickersgraver@gmail.com>
2017-12-14 15:55:07 +0000

    fixed bug that sped up flashing animation and countdown timer with every click on 'start game'

--- js/script.js
+++ js/script.js
@@ -13,30 +13,31 @@ $(()=> {
   const blueFlashColors = ['blue-flash-yellow', 'blue-flash-red', 'blue-flash-blue', 'blue-flash-green'];
   const greenFlashColors = ['green-flash-yellow', 'green-flash-red', 'green-flash-blue', 'green-flash-green'];
   const yellowFlashColors = ['yellow-flash-yellow', 'yellow-flash-red', 'yellow-flash-blue', 'yellow-flash-green'];
-  // let timerCountdown = false;
   let gameRunning = false;
-  let time = 10;
+  let time = 60;
   let lives = 3;
+  let score = 0;
   $('.end-popup1').css('visibility', 'hidden');
   $('.end-popup2').css('visibility', 'hidden');
   // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  // 2. FLASH SMALL TRIANGLES --- ON START CLICK FUNCTION+++++++++++++++++++++
+  // FLASH SMALL TRIANGLES --- ON START CLICK FUNCTION++++++++++++++++++++++++
   // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
   function flashInterval(){
-    const interval = setInterval(function(){
-      const redRandomColor = redFlashColors[Math.floor(Math.random() * 4)];
-      const blueRandomColor = blueFlashColors[Math.floor(Math.random() * 4)];
-      const greenRandomColor = greenFlashColors[Math.floor(Math.random() * 4)];
-      const yellowRandomColor = yellowFlashColors[Math.floor(Math.random() * 4)];
-      $('.flashing-red').toggleClass(redRandomColor);
-      $('.flashing-blue').toggleClass(blueRandomColor);
-      $('.flashing-green').toggleClass(greenRandomColor);
-      $('.flashing-yellow').toggleClass(yellowRandomColor);
-      if (time === 1) {
-        clearInterval(interval);
-      }
-    }, 500);
+    if (gameRunning) {
+      const interval = setInterval(function(){
+        const redRandomColor = redFlashColors[Math.floor(Math.random() * 4)];
+        const blueRandomColor = blueFlashColors[Math.floor(Math.random() * 4)];
+        const greenRandomColor = greenFlashColors[Math.floor(Math.random() * 4)];
+        const yellowRandomColor = yellowFlashColors[Math.floor(Math.random() * 4)];
+        $('.flashing-red').toggleClass(redRandomColor);
+        $('.flashing-blue').toggleClass(blueRandomColor);
+        $('.flashing-green').toggleClass(greenRandomColor);
+        $('.flashing-yellow').toggleClass(yellowRandomColor);
+        if (time === 1) {
+          clearInterval(interval);
+        }
+      }, 500);
+    }
   }
 
   function countdown(){
@@ -56,14 +57,13 @@ $(()=> {
   // 1. HIDE INSTRUCTIONS --- ON START CLICK FUNCTION++++++++++++++++++++++++++
   // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   $startgame.on('click', function() {
-    if (!gameRunning) {
-      (gameRunning = !gameRunning);
-    }
+    gameRunning = true;
     if (gameRunning) {
       $instr.hide();
     }
     countdown();
     flashInterval();
+    $startgame.off('click'); //RUN ONCE
   });
 
   // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -76,7 +76,6 @@ $(()=> {
   // SCORE SYSTEM FUNCTIONALITY++++++++++++++++++++++++++++++++++++++++++++++++
   // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-  let score = 0;
 
   function foundMatchingColors() {
     if(gameRunning){
@@ -85,41 +84,37 @@ $(()=> {
     }
 
   }
-
+  // if (time === 0 || lives === 0) {
   $restart.on('click', function () {
     gameRunning = true;
     lives = 3;
     $lives.html(lives);
     $scorenumber.html(0);
-    time = 10;
-    $timer.html(10);
+    time = 60;
+    $timer.html(60);
     countdown();
     flashInterval();
     $('.end-popup1').css('visibility', 'hidden');
     $('.end-popup2').css('visibility', 'hidden');
   });
-
-  // +++++++++++++ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  // LIVES SYSTE M FUNCTIONALITY++++++++++++++++++++++++++++++++++++++++++++++++
-  // +++++++++++ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-
+  // }
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // LOSE A LIFE FUNCTIONALITY+++++++++++++++++++++++++++++++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   function loseALife() {
-    if(lives > 0 && gameRunning){ //added &&gameRunning
+    if(lives > 0 && gameRunning){
       lives -=1;
     }
     $lives.html(lives);
     if (lives === 0) {
-      gameRunning = false; //newnewnew
+      gameRunning = false;
       $('.end-popup2').html('<p>oh dear, you ran out of lives. you scored' + ' ' + $scorenumber.html() + ' ' + 'points</p>');
       $('.end-popup2').css('visibility', 'visible');
     }
   }
   // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  // CHECK EACH SEPARATE CORNER FOR MATCHES++++++++++++++++++++++++++++++++++++
+  // CHECK FLASHING TRIANGLES FOR MATCHES. MATCH = +SCORE. NO MATCH = -LIFE++++
   // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
   function checkMatch(colour){
     const match = $(`.flashing-${colour}`).hasClass(`${colour}-flash-${colour}`);
     if (match && gameRunning) {
