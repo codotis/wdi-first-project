8d21eb8e6e8997911e08c6abf09955dc5d0259fe
Otis <otisvickersgraver@gmail.com>
2017-12-14 14:40:09 +0000

    fixed bug preventing timer from ticking and game start running when timer reached 0

--- js/script.js
+++ js/script.js
@@ -1,46 +1,30 @@
 $(()=> {
 
-  const $instrhide = $('.instr-popup');
+  const $instr = $('.instr-popup');
   const $startgame = $('#start');
   const $lives = $('#lives');
   const $timer = $('#time');
-  const $restart = $('#restart')
+  const $restart = $('#restart');
   const $scorenumber = $('#scorenumber');
-  const $flashingRed = $('.flashing-red');
-  const $flashingBlue = $('.flashing-blue');
-  const $flashingGreen = $('.flashing-green');
-  const $flashingYellow = $('.flashing-yellow');
-  let flashInterval = null;
-  let timerCountdown = false;
-  let gameRunning = false;
-  let time = 60;
-  let lives = 3;
-  $('.end-popup1').css('visibility', 'hidden');
-  $('.end-popup2').css('visibility', 'hidden');
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// INNER TRIANGLES RANDOMLY FLASH++++++++++++++++++++++++++++++++++++++++++++
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // CONSTANTS FOR INNER TRIANGLES RANDOMLY FLASHING+++++++++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   const redFlashColors = ['red-flash-yellow', 'red-flash-red', 'red-flash-blue', 'red-flash-green'];
   const blueFlashColors = ['blue-flash-yellow', 'blue-flash-red', 'blue-flash-blue', 'blue-flash-green'];
   const greenFlashColors = ['green-flash-yellow', 'green-flash-red', 'green-flash-blue', 'green-flash-green'];
   const yellowFlashColors = ['yellow-flash-yellow', 'yellow-flash-red', 'yellow-flash-blue', 'yellow-flash-green'];
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// INSTRUCTIONS MENU HIDES+++++++++++++++++++++++++++++++++++++++++++++++++++
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // let timerCountdown = false;
+  let gameRunning = false;
+  let time = 10;
+  let lives = 3;
+  $('.end-popup1').css('visibility', 'hidden');
+  $('.end-popup2').css('visibility', 'hidden');
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // 2. FLASH SMALL TRIANGLES --- ON START CLICK FUNCTION+++++++++++++++++++++
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-  $startgame.on('click', function() {
-
-    if (gameRunning === false) {
-      (gameRunning = !gameRunning);
-    }
-    if (gameRunning === true) {
-      $($instrhide).hide();
-    }
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// INNER TRIANGLES RANDOMLY FLASH++++++++++++++++++++++++++++++++++++++++++++
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    flashInterval = setInterval(function(){
+  function flashInterval(){
+    const interval = setInterval(function(){
       const redRandomColor = redFlashColors[Math.floor(Math.random() * 4)];
       const blueRandomColor = blueFlashColors[Math.floor(Math.random() * 4)];
       const greenRandomColor = greenFlashColors[Math.floor(Math.random() * 4)];
@@ -50,31 +34,47 @@ $(()=> {
       $('.flashing-green').toggleClass(greenRandomColor);
       $('.flashing-yellow').toggleClass(yellowRandomColor);
       if (time === 1) {
-        clearInterval(flashInterval);
+        clearInterval(interval);
       }
     }, 500);
+  }
 
-
-
-    if (timerCountdown === false) {
-      (timerCountdown = !timerCountdown);
-      if (timerCountdown === true) {
-        const timerStop = setInterval(function(){
-          time--;
-          $timer.html(time);
-          if (time === 0) {
-            $('.end-popup1').html('<p>time ran out! you scored' + ' ' + $scorenumber.html() + ' ' + 'points</p>');
-            $('.end-popup1').css('visibility', 'visible');
-            clearInterval(timerStop);
-          }
-        }, 1000);
+  function countdown(){
+    const timerStop = setInterval(function(){
+      time--;
+      $timer.html(time);
+      if (time === 0) {
+        $('.end-popup1').html('<p>time ran out! you scored' + ' ' + $scorenumber.html() + ' ' + 'points</p>');
+        $('.end-popup1').css('visibility', 'visible');
+        clearInterval(timerStop);
       }
+    }, 1000);
+  }
+
+
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // 1. HIDE INSTRUCTIONS --- ON START CLICK FUNCTION++++++++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  $startgame.on('click', function() {
+    if (!gameRunning) {
+      (gameRunning = !gameRunning);
     }
+    if (gameRunning) {
+      $instr.hide();
+    }
+    countdown();
+    flashInterval();
   });
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// SCORE SYSTEM FUNCTIONALITY++++++++++++++++++++++++++++++++++++++++++++++++
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // 3. START TIMER COUNTDOWN --- ON START CLICK FUNCTION++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // SCORE SYSTEM FUNCTIONALITY++++++++++++++++++++++++++++++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
   let score = 0;
 
@@ -87,23 +87,26 @@ $(()=> {
   }
 
   $restart.on('click', function () {
-    gameRunning === true;
+    gameRunning = true;
     lives = 3;
     $lives.html(lives);
     $scorenumber.html(0);
-    time = 60;
+    time = 10;
+    $timer.html(10);
+    countdown();
+    flashInterval();
     $('.end-popup1').css('visibility', 'hidden');
     $('.end-popup2').css('visibility', 'hidden');
   });
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// LIVES SYSTEM FUNCTIONALITY++++++++++++++++++++++++++++++++++++++++++++++++
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // +++++++++++++ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // LIVES SYSTE M FUNCTIONALITY++++++++++++++++++++++++++++++++++++++++++++++++
+  // +++++++++++ +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 
   function loseALife() {
-    if(lives > 0){
+    if(lives > 0 && gameRunning){ //added &&gameRunning
       lives -=1;
     }
     $lives.html(lives);
@@ -113,9 +116,9 @@ $(()=> {
       $('.end-popup2').css('visibility', 'visible');
     }
   }
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// CHECK EACH SEPARATE CORNER FOR MATCHES++++++++++++++++++++++++++++++++++++
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // CHECK EACH SEPARATE CORNER FOR MATCHES++++++++++++++++++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
   function checkMatch(colour){
     const match = $(`.flashing-${colour}`).hasClass(`${colour}-flash-${colour}`);
@@ -123,9 +126,9 @@ $(()=> {
       foundMatchingColors();
     } else loseALife();
   }
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// KEY PRESSES THAT CHECK FOR COLOUR MATCH+++++++++++++++++++++++++++++++++++
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // KEY PRESSES THAT CHECK FOR COLOUR MATCH+++++++++++++++++++++++++++++++++++
+  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
   $(window).keyup(function(e) {
     e.preventDefault();
@@ -142,7 +145,4 @@ $(()=> {
       checkMatch('green');
     }
   });
-
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
 });
